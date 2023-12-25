57ba83f308aca15bb0c3721f9fdf76ff7b75331b
irshad500 <mohammedirshadkp500@gmail.com>
2023-12-25 21:42:59 +0530

    upgraded

--- lib/data/url.dart
+++ lib/data/url.dart
@@ -1,10 +1,6 @@
 class Url {
-  // Companies
   static const firstlogic = 'https://firstlogicmetalab.com/';
-  static const calicut =
-      "https://drive.google.com/file/d/1J0YV0I9XZeuoDvVhGyupPymRE45WBBiQ/view?usp=drivesdk";
-
-  // Technologies
+  static const calicut = "https://drive.google.com/file/d/1J0YV0I9XZeuoDvVhGyupPymRE45WBBiQ/view?usp=drivesdk";
   static const react = "https://reactjs.org/";
   static const reactNative = "https://reactnative.dev/";
   static const css = "https://www.w3.org/Style/CSS/Overview.en.html";
@@ -12,13 +8,10 @@ class Url {
   static const csharp = "https://docs.microsoft.com/en-us/dotnet/csharp/";
   static const restfulApi = "https://restfulapi.net/";
   static const flutter = "https://flutter.dev/";
-  static const firebaseMessaging =
-      "https://firebase.google.com/docs/cloud-messaging";
+  static const firebaseMessaging = "https://firebase.google.com/docs/cloud-messaging";
   static const email = "mailto:mohammedirshadkp500@gmail.com";
   static const github = "https://github.com/mohammedirshadkp";
   static const instagram = "https://www.instagram.com/irshadkp_";
-  static const linkedin =
-      "https://www.linkedin.com/in/mohammed-irshad-a652b1298";
-  static const resume =
-      "https://drive.google.com/file/d/16C9bRf3l9FD8oxvTa7VRxzwj0rcRIMnm/view?usp=drivesdk";
+  static const linkedin = "https://www.linkedin.com/in/mohammed-irshad-a652b1298";
+  static const resume = "https://drive.google.com/file/d/16C9bRf3l9FD8oxvTa7VRxzwj0rcRIMnm/view?usp=drivesdk";
 }
